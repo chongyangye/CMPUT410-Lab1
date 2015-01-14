4279364272c4431c3647ee98aab26e2a0d6737ea
chongyangye <cye2@ualberta.ca>
2015-01-14 10:42:53 -0700

    finish python program

---
+++ name-family.py
@@ -0,0 +1,30 @@
+class Student:
+    courseMarks={}
+    name= ""    
+    def __init__(self,name,family):
+        self.name = name
+        self.family = family
+       
+    def addCourseMark(self, course, mark):
+        self.courseMarks[course] = mark
+    
+    def average(self):
+        grade= []
+        grade= self.courseMarks.values()
+        averageGrade= sum(grade)/float(len(grade))
+        print("The course average for %s is %.2f" %(self.name, averageGrade))
+    
+    
+    
+    
+if __name__ == "__main__":
+    student = Student("Jeff", "Hub")
+    student.addCourseMark("CMPUT410", 90)
+    student.addCourseMark("CMPUT379", 95)
+    student.addCourseMark("CMPUT466", 80)
+    student.average()
+    
+    
+    
+    
+
